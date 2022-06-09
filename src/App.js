13ffde7731945152a51d92557dc564b7f2388b7d
Petr <petromars444@gmail.com>
2022-06-09 15:31:36 +0300

    header init

--- src/App.js
+++ src/App.js
@@ -6,11 +6,116 @@ import {
 } from "react-router-dom";
 
 import {Button} from "primereact/button";
+import { Menubar } from 'primereact/menubar';
+
 import MainPage from "./pages/MainPage";
 import aboutPage from "./pages/GaleryPage";
 import Page3 from "./pages/Page3";
 import Profile from "./pages/Profile";
 import AuthPage from "./pages/AuthPage";
+
+
+
+const items = [
+    {
+        label:'Главная',
+        icon:'pi pi-fw pi-home',
+    },
+    {
+        label:'Edit',
+        icon:'pi pi-fw pi-pencil',
+        items:[
+            {
+                label:'Left',
+                icon:'pi pi-fw pi-align-left'
+            },
+            {
+                label:'Right',
+                icon:'pi pi-fw pi-align-right'
+            },
+            {
+                label:'Center',
+                icon:'pi pi-fw pi-align-center'
+            },
+            {
+                label:'Justify',
+                icon:'pi pi-fw pi-align-justify'
+            },
+
+        ]
+    },
+    {
+        label:'Users',
+        icon:'pi pi-fw pi-user',
+        items:[
+            {
+                label:'New',
+                icon:'pi pi-fw pi-user-plus',
+
+            },
+            {
+                label:'Delete',
+                icon:'pi pi-fw pi-user-minus',
+
+            },
+            {
+                label:'Search',
+                icon:'pi pi-fw pi-users',
+                items:[
+                    {
+                        label:'Filter',
+                        icon:'pi pi-fw pi-filter',
+                        items:[
+                            {
+                                label:'Print',
+                                icon:'pi pi-fw pi-print'
+                            }
+                        ]
+                    },
+                    {
+                        icon:'pi pi-fw pi-bars',
+                        label:'List'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label:'Events',
+        icon:'pi pi-fw pi-calendar',
+        items:[
+            {
+                label:'Edit',
+                icon:'pi pi-fw pi-pencil',
+                items:[
+                    {
+                        label:'Save',
+                        icon:'pi pi-fw pi-calendar-plus'
+                    },
+                    {
+                        label:'Delete',
+                        icon:'pi pi-fw pi-calendar-minus'
+                    },
+
+                ]
+            },
+            {
+                label:'Archieve',
+                icon:'pi pi-fw pi-calendar-times',
+                items:[
+                    {
+                        label:'Remove',
+                        icon:'pi pi-fw pi-calendar-minus'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label:'Quit',
+        icon:'pi pi-fw pi-power-off'
+    }
+];
 
 
 
@@ -45,7 +150,17 @@ function App() {
 
   return (
       <Router>
-                <Button label={'grewgrewg'}/>
+
+              <Menubar
+                  className='flex-shrink-0 flex-grow-1'
+                  model={items}
+                  start={<div>Logo</div>}
+              />
+
+
+
+
+
               <Switch>
                   {appRoutes.map(({path, Component, exact},index) => {
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'primeflex/primeflex.min.css'
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+
 
 
 
